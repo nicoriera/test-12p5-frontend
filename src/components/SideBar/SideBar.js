b78f424356eed5eba4d51cef1985b9012f031494
nicolas riera <nicolariera@gmail.com>
2023-07-26 23:15:51 +0200

    feat js sidebar click and hide text on desktop click on hamburger icon and show sidebar on mobil

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,51 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const SideBar = () => {
+const SideBar = ({ isOpen }) => {
+  const [hideText, setHideText] = useState(false);
+
+  const handleHeaderClick = () => {
+    setHideText(!hideText);
+  };
+
   return (
-    <div className="sidebar">
-      <div className="sidebar_header">
+    <div className={isOpen ? "sidebar open" : "sidebar"}>
+      <div className="sidebar_header" onClick={handleHeaderClick}>
         <FontAwesomeIcon icon="fa-solid fa-gauge-high" />
-        <h1>Dashboard</h1>
+        {hideText ? null : <h1>Dashboard</h1>}
       </div>
       <ul className="sidebar_links_project">
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-house" />
-          <li>Home</li>
+          {hideText ? null : <li>Home</li>}
         </div>
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-address-card" />
-          <li>Profile</li>
+          {hideText ? null : <li>Profile</li>}
         </div>
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-message" />
-          <li>Messages</li>
+          {hideText ? null : <li>Messages</li>}
         </div>
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-clock" />
-          <li>History</li>
+          {hideText ? null : <li>History</li>}
         </div>
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-list-check" />
-          <li>Tasks</li>
+          {hideText ? null : <li>Tasks</li>}
         </div>
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-people-group" />
-          <li>Communities</li>
+          {hideText ? null : <li>Communities</li>}
         </div>
       </ul>
       <ul className="sidebar_links_settings">
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-gear" />
-          <li>Settings</li>
+          {hideText ? null : <li>Settings</li>}
         </div>
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-circle-question" />
-          <li>Support</li>
+          {hideText ? null : <li>Support</li>}
         </div>
         <div className="sidebar_link">
           <FontAwesomeIcon icon="fa-shield" />
-          <li>Privacy</li>
+          {hideText ? null : <li>Privacy</li>}
         </div>
       </ul>
     </div>
