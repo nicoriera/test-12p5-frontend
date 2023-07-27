1323132dff61b41e0a6d23d5c269354eb88fdc94
nicolas riera <nicolariera@gmail.com>
2023-07-27 09:51:21 +0200

    feat delete card and create file data.json for use map in card container

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Card = () => {
+const Card = ({ removeCard, title, description }) => {
   return (
     <div className="card">
       <div className="card_background_white">
         <div className="card_header">
-          <h2>Project 1</h2>
-          <p>
-            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
-        <div className="card_footer_left">
-          <FontAwesomeIcon icon="fa-star" />
-          <FontAwesomeIcon icon="fa-eye" />
-          <FontAwesomeIcon icon="fa-share-nodes" />
+        <div className="card_footer">
+          <div className="card_footer_right">
+            <FontAwesomeIcon icon="fa-trash" onClick={removeCard} />
+          </div>
+          <div className="card_footer_left">
+            <FontAwesomeIcon icon="fa-star" />
+            <FontAwesomeIcon icon="fa-eye" />
+            <FontAwesomeIcon icon="fa-share-nodes" />
+          </div>
         </div>
       </div>
     </div>
--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card/Card";
+import cards from "../../data/data.json";
 
 const CardsContainer = () => {
+  const [cardsData, setCardsData] = useState(cards);
+
+  const removeCard = (id) => {
+    setCardsData((prevCards) => prevCards.filter((card) => card.id !== id));
+  };
+
   return (
     <div>
       <h1>Your projects</h1>
       <div className="card_container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {cardsData.map((card) => (
+          <Card
+            key={card.id}
+            title={card.title}
+            description={card.description}
+            removeCard={() => removeCard(card.id)}
+          />
+        ))}
       </div>
     </div>
   );
